2e373488d257922abee36dd56804b7380091c638
Vladimir <vkolosov1@mail.ru>
2025-12-30 14:51:49 +0300

    463

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 | # | Title | Solution | Difficulty |
 |---|-------|----------|------------|
+|463|[Find All Numbers Disappeared in an Arra](https://leetcode.com/problems/island-perimeter/)|[Java](https://github.com/m1tra/leetcode/blob/main/tasks/easy/463/Solution.java)|Easy|
 |455|[Assign Cookies](https://leetcode.com/problems/assign-cookies/)|[Java](https://github.com/m1tra/leetcode/tree/main/tasks/easy/455)|Easy|
 |448|[Find All Numbers Disappeared in an Arra](https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/)|[Java](https://github.com/m1tra/leetcode/blob/main/tasks/easy/448/Solution2.java)|Easy|
---
+++ tasks/easy/463/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+//4ms
+public class Solution {
+    public static void main(String[] args) {
+        int[][] grid = {{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}};
+
+        Solution solve = new Solution();
+        
+        System.out.println(solve.islandPerimeter(grid));
+    }
+
+    public int islandPerimeter(int[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[i].length; j++){
+                int current_cell = grid[i][j];
+                if (current_cell==1){
+                    count+=4;
+                    if (i > 0 && grid[i-1][j] == 1){
+                        count--;
+                    }
+                    if (i < grid.length-1 && grid[i+1][j] == 1){
+                        count--;
+                    }
+                    if (j > 0 && grid[i][j-1] == 1){
+                        count--;
+                    }
+                    if (j < grid[i].length -1  && grid[i][j+1] == 1){
+                        count--;
+                    }
+
+                   
+                }
+                
+            }
+        }
+
+        return count;       
+    }
+}
+
